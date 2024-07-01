b5f6680f779ef29f2e57513410fd85eeede2a0a8
NicholaiNora <nicholai.nora.dict@gmail.com>
2024-07-02 06:03:54 +0800

    React Homework 4 - Phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,42 +1,31 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import ContactForm from './ContactForm/ContactForm';
 import SearchFilter from './SearchFilter/SearchFilter';
 import ContactList from './ContactList/ContactList';
 
-export class App extends Component {
-  state = {
-    contacts: [
+const initialContacts = () => {
+  if (localStorage.getItem('contacts') !== null) {
+    return JSON.parse(localStorage.getItem('contacts'));
+  } else {
+    return [
       { id: 'id-1', name: 'Jennie Kim', number: '459-12-56' },
       { id: 'id-2', name: 'Kim Jisoo', number: '443-89-12' },
       { id: 'id-3', name: 'Im Nayeon', number: '645-17-79' },
       { id: 'id-4', name: `Mary Kris Malenab`, number: '227-91-26' },
-    ],
-    filter: '',
-  };
-
-  componentDidUpdate(_prevProps, prevState) {
-    const { contacts } = this.state;
-    if (contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(contacts));
-    }
+    ];
   }
+};
 
-  componentDidMount() {
-    if (localStorage.getItem('contacts') !== null) {
-      this.setState({ contacts: JSON.parse(localStorage.getItem('contacts')) });
-    } else {
-      this.setState({
-        contacts: [
-          { id: 'id-1', name: 'Jennie Kim', number: '459-12-56' },
-          { id: 'id-2', name: 'Kim Jisoo', number: '443-89-12' },
-          { id: 'id-3', name: 'Im Nayeon', number: '645-17-79' },
-          { id: 'id-4', name: `Mary Kris Malenab`, number: '227-91-26' },
-        ],
-      });
-    }
-  }
+function App() {
+  const [contacts, setContacts] = useState(initialContacts);
+  const [filter, setFilter] = useState('');
 
-  toCapitalize = phrase => {
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const toCapitalize = phrase => {
     return phrase
       .toLowerCase()
       .split(' ')
@@ -44,13 +33,11 @@ export class App extends Component {
       .join(' ');
   };
 
-  toFilter = phrase => {
-    this.setState({
-      filter: phrase,
-    });
+  const toFilter = phrase => {
+    setFilter(phrase);
   };
-  filterContacts = () => {
-    const { contacts, filter } = this.state;
+
+  const filterContacts = () => {
     return contacts.filter(
       contact =>
         contact.name.toLowerCase().includes(filter.toLowerCase()) ||
@@ -58,43 +45,138 @@ export class App extends Component {
     );
   };
 
-  deleteContact = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
+  const deleteContact = id => {
+    setContacts(contacts.filter(contact => contact.id !== id));
+    // setContacts(prevContacts => {
+    //   const updatedContacts = prevContacts.filter(contact => contact.id !== id);
+    //   // console.log('Updated contacts after deleting:', updatedContacts);
+    //   return updatedContacts;
+    // });
   };
 
-  addContact = newContact => {
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, newContact],
-    }));
+  const addContact = newContact => {
+    // setContacts([...contacts, newContact]);
+    setContacts(prevContacts => {
+      const updatedContacts = [...prevContacts, newContact];
+      // console.log('Updated contacts after adding:', updatedContacts);
+      return updatedContacts;
+    });
   };
 
-  reset = () => {
-    this.setState({ name: '', number: '' });
-  };
-
-  render() {
-    const { contacts, filter } = this.state;
-    return (
-      <div>
-        <h2>Phonebook</h2>
-        <ContactForm
-          addContact={this.addContact}
-          reset={this.reset}
-          contacts={contacts}
-          toCapitalize={this.toCapitalize}
-        />
-        <h2>Contacts</h2>
-        <SearchFilter filter={filter} toFilter={this.toFilter} />
-        <ContactList
-          filterContacts={this.filterContacts}
-          deleteContact={this.deleteContact}
-          toCapitalize={this.toCapitalize}
-        />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <ContactForm
+        addContact={addContact}
+        contacts={contacts}
+        toCapitalize={toCapitalize}
+      />
+      <h2>Contacts</h2>
+      <SearchFilter filter={filter} toFilter={toFilter} />
+      <ContactList
+        filterContacts={filterContacts}
+        deleteContact={deleteContact}
+        toCapitalize={toCapitalize}
+      />
+    </div>
+  );
 }
 
 export default App;
+
+// export class App extends Component {
+//   state = {
+//     contacts: [
+//       { id: 'id-1', name: 'Jennie Kim', number: '459-12-56' },
+//       { id: 'id-2', name: 'Kim Jisoo', number: '443-89-12' },
+//       { id: 'id-3', name: 'Im Nayeon', number: '645-17-79' },
+//       { id: 'id-4', name: `Mary Kris Malenab`, number: '227-91-26' },
+//     ],
+//     filter: '',
+//   };
+
+//   componentDidUpdate(_prevProps, prevState) {
+//     const { contacts } = this.state;
+//     if (contacts !== prevState.contacts) {
+//       localStorage.setItem('contacts', JSON.stringify(contacts));
+//     }
+//   }
+
+//   componentDidMount() {
+//     if (localStorage.getItem('contacts') !== null) {
+//       this.setState({ contacts: JSON.parse(localStorage.getItem('contacts')) });
+//     } else {
+//       this.setState({
+//         contacts: [
+//           { id: 'id-1', name: 'Jennie Kim', number: '459-12-56' },
+//           { id: 'id-2', name: 'Kim Jisoo', number: '443-89-12' },
+//           { id: 'id-3', name: 'Im Nayeon', number: '645-17-79' },
+//           { id: 'id-4', name: `Mary Kris Malenab`, number: '227-91-26' },
+//         ],
+//       });
+//     }
+//   }
+
+//   toCapitalize = phrase => {
+//     return phrase
+//       .toLowerCase()
+//       .split(' ')
+//       .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+//       .join(' ');
+//   };
+
+//   toFilter = phrase => {
+//     this.setState({
+//       filter: phrase,
+//     });
+//   };
+
+//   filterContacts = () => {
+//     const { contacts, filter } = this.state;
+//     return contacts.filter(
+//       contact =>
+//         contact.name.toLowerCase().includes(filter.toLowerCase()) ||
+//         contact.number.includes(filter)
+//     );
+//   };
+
+//   deleteContact = id => {
+//     this.setState(prevState => ({
+//       contacts: prevState.contacts.filter(contact => contact.id !== id),
+//     }));
+//   };
+
+//   addContact = newContact => {
+//     this.setState(prevState => ({
+//       contacts: [...prevState.contacts, newContact],
+//     }));
+//   };
+
+//   reset = () => {
+//     this.setState({ name: '', number: '' });
+//   };
+
+//   render() {
+//     const { contacts, filter } = this.state;
+//     return (
+//       <div>
+//         <h2>Phonebook</h2>
+//         <ContactForm
+//           addContact={this.addContact}
+//           reset={this.reset}
+//           contacts={contacts}
+//           toCapitalize={this.toCapitalize}
+//         />
+//         <h2>Contacts</h2>
+//         <SearchFilter filter={filter} toFilter={this.toFilter} />
+//         <ContactList
+//           filterContacts={this.filterContacts}
+//           deleteContact={this.deleteContact}
+//           toCapitalize={this.toCapitalize}
+//         />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,30 +1,55 @@
 import ContactListItem from 'components/ContactListItem/ContactListItem';
-import React, { Component } from 'react';
+import React from 'react';
+// import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import css from './ContactList.module.css';
 
-export class ContactList extends Component {
-  static propTypes = {
+function ContactList({ filterContacts, toCapitalize, deleteContact }) {
+  const filteredContacts = filterContacts();
+  return (
+    <ul className={css.contactList}>
+      {filteredContacts.map(contact => (
+        <ContactListItem
+          key={contact.id}
+          toCapitalize={toCapitalize}
+          deleteContact={deleteContact}
+          contact={contact}
+        />
+      ))}
+    </ul>
+  );
+}
+
+ContactList.propTypes = {
     filterContacts: PropTypes.func.isRequired,
     toCapitalize: PropTypes.func.isRequired,
     deleteContact: PropTypes.func.isRequired,
   };
-  render() {
-      const { filterContacts, toCapitalize, deleteContact } = this.props;
-      const filteredContacts = filterContacts();
-    return (
-      <ul className={css.contactList} >
-        {filteredContacts.map(contact => (
-          <ContactListItem
-            key={contact.id}
-            toCapitalize={toCapitalize}
-            deleteContact={deleteContact}
-            contact={contact}
-          />
-        ))}
-      </ul>
-    );
-  }
-}
 
 export default ContactList;
+
+// export class ContactList extends Component {
+//   static propTypes = {
+//     filterContacts: PropTypes.func.isRequired,
+//     toCapitalize: PropTypes.func.isRequired,
+//     deleteContact: PropTypes.func.isRequired,
+//   };
+//   render() {
+//       const { filterContacts, toCapitalize, deleteContact } = this.props;
+//       const filteredContacts = filterContacts();
+//     return (
+//       <ul className={css.contactList} >
+//         {filteredContacts.map(contact => (
+//           <ContactListItem
+//             key={contact.id}
+//             toCapitalize={toCapitalize}
+//             deleteContact={deleteContact}
+//             contact={contact}
+//           />
+//         ))}
+//       </ul>
+//     );
+//   }
+// }
+
+// export default ContactList;
